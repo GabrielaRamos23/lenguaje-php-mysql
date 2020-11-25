8bd87c41cfa4ba9d73b18beb16ebbdc70099db71
GabrielaRamos23 <gabrielaramosch23@gmail.com>
2020-11-24 22:01:16 -0500

    resubiendo codigos

--- index.php
+++ index.php
@@ -11,19 +11,45 @@ include 'conexion/index.php';
 </head>
 <body>
 	<?php
+	/*
 		$var1 = 12;
 		echo $var1;
+
 		$var2 = "hola";
 		echo $var2;
+
 		$var3 = 14.12;
 		echo $var3;
+
 		$var4 = $var1.$var2;
 		echo $var4;
+
 		$var5 = $var4;
 		echo $var5;
 		
-		
-		
+		*/
+		$var = 1;
+		switch ($var) {
+			case '1':
+				# code...
+				echo "uno";
+				break;
+			case '2':
+				echo "dos";
+				break;
+			case '3':
+				echo "tres";
+				break;
+			case'4':
+				echo "cuatro";
+				break;
+			case '5':
+				echo "cinco";
+				break;
+			default :
+				echo "Hello";
+				break;
+		}
 		
 			?>
 	
